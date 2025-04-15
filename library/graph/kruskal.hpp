af5edee219fa73aee29c3524af479a2a02e3005c
poe <153431867+mackerel38@users.noreply.github.com>
2025-04-15 02:12:46 +0000

    poe

--- library/graph/kruskal.hpp
+++ library/graph/kruskal.hpp
@@ -5,13 +5,13 @@
 using namespace std;
 template<class T=int>
 graph<T> kruskal(graph<T>& g) {
-    edges<T> _edges = g.getedges();
+    edges<T> _edges = g._edges;
     graph<T> re(g.size(), false, true);
     sort(_edges.begin(), _edges.end(), [](edge<T>& a, edge<T>& b) { return a.cost < b.cost; });
     UnionFind uf(g.size());
     for (auto& _e : _edges) {
         if (uf.merge(_e.from, _e.to)) {
-            re.add_edge(_e.id);
+            re.add_edge(_e);
         }
     }
     return re;
