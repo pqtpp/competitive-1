6cbb7abfb552ac88f5141faaa2ec410901d08361
poe <153431867+mackerel38@users.noreply.github.com>
2025-04-18 13:57:49 +0000

    Update markdown docs for GitHub Pages

--- atcoder/abc149/a/main.cpp
+++ atcoder/abc149/a/main.cpp
@@ -1,20 +1,28 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/enumerate_primes"
 #include "template"
-#include "prime"
+#include "rollinghash"
 
 int main() { IO();
     int T=1;
     // cin >> T;
     while (T--) solve();
 }
+string shortest_palindrome(string S) {
+    string R = S;
+    reverse(R.begin(), R.end());
+
+    rollinghash hS(S), hR(R);
+    int n = S.size(), len = 0;
+    for (int i = 0; i <= n; ++i) {
+        // check if S[i..n) == reverse(R[0..n-i))
+        if (hS.prod(i, n) == hR.prod(0, n - i)) {
+            len = n - i;
+            break;
+        }
+    }
+    return R.substr(0, n - len) + S;
+}
 
 void solve() {
-    ll n, a, b; cin >> n >> a >> b;
-    vi primes;
-    enumprimes(n, primes);
-    int cnt = primes.size();
-    vi ans;
-    for (int i=b; i<cnt; i+=a) ans.pb(primes[i]);
-    cout << cnt << sp << ans.size() << nl;
-    cout << ans;
+    string s = "aacecaaa";
+    cout << shortest_palindrome(s) << endl;  // 出力: "aaacecaaa"
 }
--- library/string/rollinghash.hpp
+++ library/string/rollinghash.hpp
@@ -4,28 +4,28 @@ using namespace std;
 struct rollinghash {
     string data;
     vector<long long> rhash, power;
-    long long r;
+    long long rn;
     rollinghash(string& s) : data(s), rhash(s.size() + 1), power(s.size() + 1) {
         static mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
         uniform_int_distribution<long long> dist(1LL << 30, ((1LL << 61) - 2));
-        r = dist(rng) | 1;
+        rn = dist(rng) | 1;
         rhash[0] = 0;
         power[0] = 1;
         for (int i=0; i<s.size(); i++) {
-            rhash[i + 1] = mod(mul(rhash[i], r) + s[i]);
-            power[i + 1] = mod(mul(power[i], r));
+            rhash[i + 1] = modul(mul(rhash[i], rn) + s[i]);
+            power[i + 1] = modul(mul(power[i], rn));
         }
     }
     long long prod(int l, int r) {
-        return mod(rhash[r] - mul(rhash[l], power[r - l]) + (1LL << 61) - 1);
+        return modul(rhash[r] - mul(rhash[l], power[r - l]) + (1LL << 61) - 1);
     }
-    long long mod(long long x) {
+    long long modul(long long x) {
         long long re = (x & ((1LL << 61) - 1)) + (x >> 61);
         if (((1LL << 61) - 1) <= re) re -= (1LL << 61) - 1;
         return re;
     }
     long long mul(long long a, long long b) {
         __int128_t t = (__int128_t)a * b;
-        return mod((t & ((1LL << 61) - 1)) + (t >> 61));
+        return modul((t & ((1LL << 61) - 1)) + (t >> 61));
     }
 };
