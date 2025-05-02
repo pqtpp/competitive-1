9747b0aa8ad8bc590d6e542cf3fa56ec7e110157
poe <153431867+mackerel38@users.noreply.github.com>
2025-05-02 04:11:52 +0000

    Update markdown docs for GitHub Pages

--- atcoder/abc000/a/bundled.cpp
+++ atcoder/abc000/a/bundled.cpp
@@ -137,6 +137,35 @@ struct graph {
         return data[k];
     }
 };
+using namespace std;
+// グラフの全点間最短経路を求める O(n^3)
+template <class T>
+vector<vector<T>> warshallfloyd(vector<vector<T>>& g) {
+    vector<vector<T>> d = g;
+    for (int k=0; k<g.size(); k++) {
+        for (int i=0; i<g.size(); i++) {
+            for (int j=0; j<g.size(); j++) {
+                if (d[i][k] < numeric_limits<T>::max()/2 && d[k][j] < numeric_limits<T>::max()/2) {
+                    d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+                }
+            }
+        }
+    }
+    return d;
+}
+// グラフの全点間最短経路を求める O(n^3)
+template<class T = int, bool directed = false, bool weighted = true>
+vector<vector<T>> warshallfloyd(graph<T, directed, weighted>& g) {
+    vector<vector<T>> d(g.size(), vector<T>(g.size(), numeric_limits<T>::max()));
+    for (int i=0; i<g.size(); i++) {
+        d[i][i] = T{};
+        for (auto& _e : g[i]) {
+            d[i][_e.to] = _e.cost;
+        }
+    }
+    return warshallfloyd(d);
+}
+
 int main() { IO();
     int T=1;
     // cin >> T;
@@ -147,20 +176,17 @@ void solve() {
     int n, m; cin >> n >> m;
     graph<ll, false, true> g(n);
     g.read(m, 1);
-    set<int> used;
-    rep(i, n) {
-        pqg<pll> q; q.push({0, i});
-        vll d(n, INF); d[i] = 0;
-        while (!q.empty()) {
-            auto [x, y] = q.top(); q.pop();
-            if (d[y] < x) continue;
-            range(e, g[y]) {
-                    if (chmin(d[e.to], d[e.from]+e.cost)) {
-                    used.insert(e.id);
-                    q.push({d[e.to], e.to});
-                }
+    vvll d = warshallfloyd(g);
+    int cnt = 0;
+    range(e, g._edges) {
+        int ans = 0;
+        int x=e.from, y=e.to, z=e.cost;
+        rep(i, n) rep(j, n) {
+            if (d[x][i]+z+d[j][y] == d[x][y]) {
+                ans = 1;
             }
         }
+        cnt += ans;
     }
-    cout << m - used.size() << nl;
+    cout << m - cnt << nl;
 }
